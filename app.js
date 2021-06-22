182e6849d97e058bab33e1d235d416383e7f73c7
Josiah Leon <sjosiahleon@gmail.com>
2021-06-21 17:23:18 -0700

    freaking crap I'm frustrated (not gonna bother spell checking)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const fetch = require('node-fetch')
+const express = require('express');
+const app = express();
+const path = require('path');
+const fetch = require('node-fetch');
+const gAPI = require('google-trends-api');
 const PORT = process.env.PORT || 3000; // process.env accesses heroku's environment variables
 
 app.use(express.static('public'))
@@ -11,29 +12,51 @@ app.get('/', (request, res) => {
 })
 
 // create route to get single book by its isbn
-app.get('/books/:isbn', (request, response) => {
-  // make api call using fetch
-  fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)   // logs to server
-      response.send(results) // sends to frontend
-    });
-});
+// app.get('/books/:isbn', (request, response) => {
+//   // make api call using fetch
+//   fetch(`http://openlibrary.org/api/books?bibkeys=ISBN:${request.params.isbn}&format=json&jscmd=data`)
+//   .then((response) => {
+//       return response.text();
+//   }).then((body) => {
+//       let results = JSON.parse(body)
+//       console.log(results)   // logs to server
+//       response.send(results) // sends to frontend
+//     });
+// });
 
+app.get('/search/:term', (req, res) => {
+  // gAPI.autoComplete({keyword: `${req.params.term}`})
+  gAPI.autoComplete({keyword: `${req.params.term}`})
+    .then(result => {
+      res.send(result);
+    })
+})
+
+
+app.get('/search/daily', (req, res) => {
+  gAPI.dailyTrends({geo: 'US'})
+    .then(result => {
+      res.send(result);
+    })
+})
+
+app.get('/IOT/:term', (req, res) => {
+  gAPI.interestOverTime({keyword: `${req.params.term}`})
+    .then(result => {
+      res.send(result);
+    })
+})
 // create a search route
-app.get('/search', (request, response) => {
-  fetch(`http://openlibrary.org/search.json?q=${request.query.string}`)
-  .then((response) => {
-      return response.text();
-  }).then((body) => {
-      let results = JSON.parse(body)
-      console.log(results)
-      response.send(results)
-    });
-});
+// app.get('/search', (request, response) => {
+//   fetch(`http://openlibrary.org/search.json?q=${request.query.string}`)
+//   .then((response) => {
+//       return response.text();
+//   }).then((body) => {
+//       let results = JSON.parse(body)
+//       console.log(results)
+//       response.send(results)
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(__dirname);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,48 +1,26 @@
 const axios = require('axios');
 const d3 = require('d3');
-const gAPI = require('google-trends-api');
+const topojson = require('topojson');
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+    let dataVis = document.getElementsByClassName('vis-div')[0];
     
+    const svg = d3.select(".vis-div").append('svg')
+        .attr('width', 800)
+        .attr('height', 800)
+        .style('background-color', '#666666')
     
 
-    gAPI.autoComplete({keyword: 'Back to School'})
-    .then(function(results) {
-      console.log(results);
-    })
-    .catch(function(err) {
-      console.error(err);
-    })
+    // let term = 'magic'
+    // axios.get(`/search/${term}`)
+    //     .then(response => {
+    //         // console.log(response);
+    //         dataVis.appendChild(document.createTextNode(response));
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //     })
 
-    // let info = document.createElement("div");
-    // info.appendChild(document.createTextNode());
-    // document.body.insertBefore(info, document.getElementById('main'))
-    
-    // let isbn = '0201558025';
-    // axios.get(`/books/${isbn}`)
-    // .then((response) => {
-    //     let info = document.createElement("div");
-
-    //     for (id in response.data) {
-    //         info.appendChild(document.createTextNode(response.data[id].number_of_pages));
-    //         console.log(response.data[id].number_of_pages);
-    //     }
-
-    //     document.body.insertBefore(info, document.getElementById('main'))
-
-    //     // console.log(response.data); 
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
-
-    // let query = "grace hopper";
-    // axios.get(`/search?string=${query}`)
-    // .then((response) => {
-    //     console.log(response);
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
-    
 })
